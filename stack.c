8f58550d01e278f0052f6ab4d4b6b1034ffc1833
mehdizit <zitounimehdi.it@gmail.com>
2020-12-21 16:57:45 +0100

    push pall function

---
+++ stack.c
@@ -0,0 +1,69 @@
+#include "monty.h"
+/**
+ * push - add new item to top of stack
+ * @stack: pointer to stack
+ * @line_number: line number of instruction
+ * @n: value of new item on stack
+ * Return: void, exit with -1 on failure
+ */
+void push(stack_t **stack, unsigned int line_number, char *n )
+{
+
+	int i;
+	stack_t *new = NULL;
+
+	if (n == NULL)
+	{
+		printf("L<%d>: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[0] == '-' && i == 0)
+			continue;
+		if (isdigit(n[i] == 0))
+		{
+			printf("L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = atoi(n);
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (*stack != NULL)
+	{
+		new->next = *stack;
+		(*stack)->prev = new;
+	}
+	*stack = new;
+}
+	
+				
+/**
+ * pall - prints the value of all items on the stack
+ * @stack: pointer to stack
+ * @line_number: line number of instruction
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int __attribute__((unused))line_number)
+{
+	stack_t *current = *stack;
+
+	if (stack == NULL || *stack == NULL)
+		return;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
