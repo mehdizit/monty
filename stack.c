35239b7fd4e79cb1cb33409ac26078cff3db4d19
mehdizit <zitounimehdi.it@gmail.com>
2020-12-21 17:54:50 +0100

    update

--- stack.c
+++ stack.c
@@ -79,7 +79,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	
 	if (stack == NULL || *stack == NULL)
 	{
-		printf("L%d: can't pint, stack empty\n", line_number);
+		printf("L<%d>: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	i = (*stack)->n;
