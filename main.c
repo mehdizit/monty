571ff071f6aa9804189aa34a92e1b7f324eaaf6f
mehdizit <zitounimehdi.it@gmail.com>
2020-12-21 17:01:03 +0100

    entred point

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+/**
+ *main - entred point
+ *@argc: integer
+ *@argv: char pointer
+ *Return: success otherwise error or failure
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	stack_t *stack = NULL;
+	char *line = NULL;
+	unsigned int line_number;
+	char *opcode;
+	char *n;
+	size_t len = 0;
+	ssize_t read;
+	
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	line_number = 0;
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+	
+		line_number++;
+	
+		opcode = strtok(line, DELIMITERS);
+		if (opcode == NULL || strncmp(opcode, "#", 1) == 0)
+			continue;
+		if (strcmp(opcode, "push") == 0)
+		{
+			n = strtok(NULL, DELIMITERS);
+			push(&stack, line_number, n);
+		}
+		else
+			opcode_struct(opcode, &stack, line_number);
+	}
+
+	free_all(stack, line, fp);
+	return (EXIT_SUCCESS);
+}
+			
