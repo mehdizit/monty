b1fadf66a659b34f63ddf713ef912bb09d309eca
mehdizit <zitounimehdi.it@gmail.com>
2020-12-22 17:48:47 +0100

    error update

--- stack.c
+++ stack.c
@@ -124,7 +124,9 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (stack_length(stack) < 2)
 	{
-		printf("L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		if (*stack)
+			free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
 
