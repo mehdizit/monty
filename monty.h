3827acbf2654026bfb8a714b064f2bed5d4e7eaf
mehdizit <zitounimehdi.it@gmail.com>
2020-12-22 17:14:24 +0100

    update modulo

--- monty.h
+++ monty.h
@@ -59,4 +59,5 @@ void free_dlistint(stack_t *head);
 void _sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 #endif
--- opcodeFunctionPointer.c
+++ opcodeFunctionPointer.c
@@ -23,6 +23,7 @@ int opcode_struct(char *opcode, stack_t **stack, unsigned int line_number)
 		{"sub", _sub},
 		{"div", _div},
 		{"mul", _mul},
+		{"mod", _mod},
 		{NULL, NULL}
 	};
 
